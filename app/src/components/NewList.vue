3be260f5617f8139d7be41a7fddbfeb694397bd4
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-01-15 14:02:18 -0500

    reactive

---
+++ app/src/components/NewList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+    </div>
+</template>
+
+<script setup>
+const animals = ['Zebra', 'Ape', 'Panda', 'Dustin']
+</script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <form>
+      <p>Username</p>
+      <input type="text" v-model="username" required>
+      <p>Password</p>
+      <input type="text" v-model="password" required>
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
+
+<script setup>
+/* import {ref, reactive} from 'vue'
+const count = ref(0)
+const user = ref('')
+function increment(){
+  count.value++
+} */
+
+import { reactive } from "vue";
+
+const user = reactive({
+  username: '',
+  password: ''
+})
+
+function getData(){
+  
+}
+</script>
+ 
+<style scoped></style>
