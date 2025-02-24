8f4f7dd3af0ad57c48b75f20f2896cd3355006cf
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-02-24 14:02:11 -0500

    Create PokemonCard.vue

---
+++ app/src/components/PokemonCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="pokemon-card">
+            <h2>{{ name }}</h2>
+            <h3>{{ index }}</h3>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    name: String,
+    index: Number
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
